ebedd95a1a82a5cae491744159e6fda902bf62a8
Diemlykt <143418329+Diemlykt@users.noreply.github.com>
2025-05-22 13:57:42 +0700

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -37,10 +37,7 @@ with tab1:
     if st.button("Predict Alzheimer’s (Clinical)"):
         try:
             # Request clinical prediction
-            for doc in db["records"].find():
-                print(doc["PatientID"])
-                print(st.session_state['clinical_patient_id'])
-            response = requests.post(f"{FASTAPI_URL}/predict/clinical", params={"patient_id": st.session_state['clinical_patient_id']})
+            response = requests.post(f"{FASTAPI_URL}/predict/clinical", params={"patient_id": int(st.session_state['clinical_patient_id'])})
             if response.status_code == 200:
                 result = response.json()
                 st.write(f"**Prediction**: {result['prediction']}")
